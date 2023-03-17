afc2be30860adb269c2dbabd0d109b05f22d5970
corysterner <corysterner@gmail.com>
2023-03-16 22:04:20 -0500

    Update p3Heap.c

--- p3/p3Heap.c
+++ p3/p3Heap.c
@@ -215,7 +215,7 @@ int bfree(void *ptr) {
 		return -1;
 	}
 	
-	blockHeader* free_header = ptr - sizeof(blockHeader);
+	blockHeader* free_header = ptr - (void*)sizeof(blockHeader);
 	
 	if (isFree(free_header)){
 		return -1;
