2c7216b0cc6fdfe6fd7a4cf553490aa670a0a071
corysterner <corysterner@gmail.com>
2023-03-16 21:13:35 -0500

    Update p3Heap.c

--- p3/p3Heap.c
+++ p3/p3Heap.c
@@ -145,7 +145,7 @@ void* balloc(int size) {
 	
 	//Initialize variables for loop
 	int curr_size;
-	blockHeader *current = heap_start - 1;
+	blockHeader *current = heap_start - sizeof(blockHeader);
 	blockHeader *best_fit = NULL;
 	
 	//Loop over all values from start to finish
